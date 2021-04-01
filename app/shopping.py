0e5a289d59c24ead9a5e9de9570fa713e94b4d31
susanna00 <77734618+susanna00@users.noreply.github.com>
2021-04-01 16:24:30 -0700

    Mock test on the shopping cart project

--- app/shopping.py
+++ app/shopping.py
@@ -14,6 +14,19 @@ def format_usd(my_price):
     return f"${my_price:,.2f}"
 
 
+def lookup_product(product_id, all_products): 
+    """
+    Params :
+        product_id (str) like "8"
+        all_products (lost of dict) each dict should have "id", "name", "department", "aisle", and "price" attribute:
+    """
+
+    matching_products = [p for p in all_products if str(p["id"]) == str(product_id)]
+    if any(matching_products): 
+        return matching_products[0]
+    else: 
+        return None 
+
 
 
 if __name__ == "__main__":
@@ -34,9 +47,9 @@ if __name__ == "__main__":
         if selected_id.upper() == "DONE":
             break
         else:
-            matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
-            if any(matching_products):
-                selected_products.append(matching_products[0])
+            matcing_product = lookup_product(selected_id, products)
+            if matching_product:
+                selected_products.append(matching_product)
             else:
                 print("OOPS, Couldn't find that product. Please try again.")
 
--- test/shopping_test.py
+++ test/shopping_test.py
@@ -1,7 +1,6 @@
-
-# TODO: import some code
-
-from app.shopping import format_usd 
+import os 
+from pandas import read_csv
+from app.shopping import format_usd, lookup_product 
 
 def test_format_usd():
     # assert 2 == 2
@@ -11,5 +10,26 @@ def test_format_usd():
 
 
 
+mock_products_filepath = os.path.join(os.path.dirname(__file__), "mock_data", "mock_products.csv")
+mock_products_df = read_csv(mock_products_filepath)
+mock_products = mock_products_df.to_dict("records")
 
-# TODO: test the code
+def test_lookups():
+    """
+    With valid product id, returns the product info below.
+    """
+    valid_result = lookup_product("8", mock_products)
+    assert valid_result == {
+        'aisle': 'Aisle C',
+        'department': 'snacks',
+        'id': 8,
+        'name': "Product 8",
+        'price': 10.0
+    }
+    """
+    With invalid product id, returns None as shown below.
+    """
+    invalid_result = lookup_product("88888888", mock_products)
+    assert invalid_result == None 
+
+
